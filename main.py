2d18b63af980675a260939304775a8d695c88658
dongyuu1 <147731990+dongyuu1@users.noreply.github.com>
2024-04-12 03:22:25 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os
-import cv2
-from A import cfgs as cfgsA
-from A import launch as launchA
+import random
 import argparse
 import torch
 import numpy as np
-import random
+from A import cfgs as cfgsA
+from A import launch as launchA
+
 
 def load_args():
     """
@@ -13,8 +13,31 @@ def load_args():
     :return: The corresponding configuration parameters
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument('--task', type=str, default='A',
-                        help='which task to perform. possible choices: A, B')
+    parser.add_argument(
+        "--task",
+        type=str,
+        default="A",
+        help="which task to perform. possible choices: A, B",
+    )
+    parser.add_argument("--mode", type=str, default="train", help="train or test")
+    parser.add_argument(
+        "--cmid", type=int, default=32, help="number of channels of the latent features"
+    )
+    parser.add_argument(
+        "--cup",
+        type=int,
+        default=24,
+        help="number of channels of the latent features in the reconstruction module",
+    )
+    parser.add_argument(
+        "--lr", type=float, default=2e-3, help="the initial learning rate"
+    )
+    parser.add_argument(
+        "--test_data",
+        type=str,
+        default="B100",
+        help="the test dataset selected, not used for training",
+    )
     args = parser.parse_args()
 
     return args
@@ -29,23 +52,27 @@ def load_config(args):
     """
     path = os.getcwd()
     # Setup cfg.
-    assert args.task == 'A' or args.task == 'B'
-    base_path = os.path.abspath('.')
-    if args.task == 'A':
+    assert args.task == "A" or args.task == "B"
+    if args.task == "A":
         cfg = cfgsA.get_cfg()
     else:
         cfg = None
 
-    cfg.LR_TRAIN_PATH = os.path.join(path, "data", "DIV2K_train_LR_bicubic", "X4")
-    cfg.HR_TRAIN_PATH = os.path.join(path, "data", "DIV2K_train_HR")
-    cfg.LR_VAL_PATH = os.path.join(path, "data", "DIV2K_valid_LR_bicubic", "X4")
-    cfg.HR_VAL_PATH = os.path.join(path, "data", "DIV2K_valid_HR")
-
+    cfg.LR_TRAIN_PATH = os.path.join(path, "Datasets", "DIV2K_train_LR_bicubic", "X4")
+    cfg.HR_TRAIN_PATH = os.path.join(path, "Datasets", "DIV2K_train_HR")
+    cfg.LR_VAL_PATH = os.path.join(path, "Datasets", "DIV2K_valid_LR_bicubic", "X4")
+    cfg.HR_VAL_PATH = os.path.join(path, "Datasets", "DIV2K_valid_HR")
+    cfg.MODEL.CMID = args.cmid
+    cfg.MODEL.CUP = args.cup
+    cfg.TRAIN.LR = args.lr
     return cfg
 
 
 def main():
-
+    """
+    The main function switching the mode
+    :return: None
+    """
     # Load the input arguments
     args = load_args()
 
@@ -63,7 +90,14 @@ def main():
     # torch.backends.cudnn.enabled = False
 
     if args.task == "A":
-       launchA.train(cfg)
-       #launchA.test(cfg, dataset="B100", trial_time=48000)
+        if args.mode == "train":
+            launchA.train(cfg)
+            launchA.test(cfg, dataset="DIV2K", trial_time=48000)
+        elif args.mode == "test":
+            launchA.test(cfg, dataset=args.test_data, trial_time=48000)
+        else:
+            launchA.visualization(cfg, dataset=args.test_data, photo_num=6)
 
-main()
+
+if __name__ == "__main__":
+    main()
